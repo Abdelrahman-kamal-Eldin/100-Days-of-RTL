c2f9d1b09cbdc6cc64d1a0e4ff16d3a85a2f69a7
Abdelrahman-kamal-Eldin <abdelrahman.kamal.8597@gmail.com>
2024-09-11 00:56:46 +0300

    Update HS_tb.v

--- 4-Half Subtractor/HS_tb.v
+++ 4-Half Subtractor/HS_tb.v
@@ -1,17 +1,17 @@
 `timescale 1 ns / 1 ps
 module HS_tb;
 reg x,y;
-wire B,D;
+wire D,B;
 
 HS Test (     // OR write HS Test(x,y,B,D);
         .x(x), 
-		.y(y), 
-		.B(B), 
-		.D(D)
+	.y(y), 
+	.D(D), 
+	.B(B)
 	);
 	
 initial begin 
-$monitor("Time=%0t: x=%b, y=%b, B=%b, D=%b",$time,x,y,B,D); 
+	$monitor("Time=%0t: x=%b, y=%b, D=%b, B=%b",$time,x,y,D,B); 
 	x=0;y=0;#5;
 	x=0;y=1;#5;
 	x=1;y=0;#5;
